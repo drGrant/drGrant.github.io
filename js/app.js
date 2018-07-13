9e350962a06a88b3636f96ac9de31e0419c7fb28
David Grant <davebrklyn1@gmail.com>
2018-07-13 04:29:00 -0400

    Added javascript

---
+++ js/app.js
@@ -0,0 +1,37 @@
+/*
+* 
+Lets have some fun............ :-)
+*/
+
+var IOIndex;
+((IOIndex, pramas) => {
+    pramas = pramas || {};
+
+    IOIndex.app = {
+        createElement: (element) => {
+            var createdElement = $("<" + element + "></" + element + ">");
+            return createdElement;
+        },
+        init: () => {
+            $(document).ready(() => {
+                var target = {
+                    sandbox: $("#sandbox"),
+                    mainStyles: $('.mainStyles'),
+                    mainImage:$('.main-image')
+                }
+                target.mainStyles.toggleClass('layout_2');
+                target.mainImage.on("click", "", (e) => {
+                    target.mainStyles.toggleClass('layout_2');
+
+
+                })
+            })
+
+        }
+    }
+    var scope = IOIndex.app;
+    IOIndex.app.init();
+    return {
+        "start": ""
+    }
+})(IOIndex || (IOIndex = {}));
