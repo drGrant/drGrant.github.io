e8ade45f5ee2056df58efbc26588f7a1477346b3
David Grant <davebrklyn1@gmail.com>
2018-07-16 12:45:59 -0400

    update 12:45 : 7-16-18

--- js/app.js
+++ js/app.js
@@ -1,34 +1,31 @@
 /*
-* 
-Lets have some fun............ :-)
+* Lets have some fun !! :-)
 */
 
 var IOIndex;
 ((IOIndex, pramas) => {
     pramas = pramas || {};
-    let createElement = (element) => {
-        var createdElement = $("<" + element + "></" + element + ">");
-        return createdElement;
-    },
-    init = ()=>{
-        var target = {
-            sandbox: $("#sandbox"),
-            mainStyles: $('.mainStyles'),
-            mainImage:$('.main-image')
+    let choosePic = Math.round(Math.random() * 1),
+        createElement = (element) => {
+            let createdElement = $("<" + element + "></" + element + ">");
+            return createdElement;
         },
-        choosePic = Math.round(Math.random()*1);
-       if(choosePic==1) target.mainStyles.toggleClass('layout_2');
-        target.mainImage.on("click", "", (e) => {
-            target.mainStyles.toggleClass('layout_2');
-        });
-
-    } 
+        init = () => {
+            let target = {
+                sandbox: $("#sandbox"),
+                mainStyles: $('.mainStyles'),
+                mainImage: $('.main-image')
+                }
+            if (choosePic == 1) target.mainStyles.toggleClass('layout_2');
+            target.mainImage.on("click", "", (e) => {
+                target.mainStyles.toggleClass('layout_2');
+            });
+        };
     IOIndex.app = {
         init: () => {
-            $(document).ready(() => { init(); })
+            $(document).ready(() => { init(); });
         }
     }
-    var appScope = IOIndex.app;
+    let appScope = IOIndex.app;
     appScope.init();
-    
 })(IOIndex || (IOIndex = {}));
